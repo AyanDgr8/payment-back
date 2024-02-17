da197a1fbf172ed2bd609b72d6959a30b42079eb
Ayan <ayandgr5@gmail.com>
2024-02-17 13:03:49 +0530

    files added

---
+++ src/middlewares/validationMiddleware.js
@@ -0,0 +1,21 @@
+// src/middlewares/validationMiddleware.js
+
+import Joi from 'joi';
+
+const userDetailsSchema = Joi.object({
+  name: Joi.string().required(),
+  phone: Joi.number().required().integer().min(1000000000).max(9999999999),
+  email: Joi.string().email().required(),
+  gender: Joi.string().valid('men', 'women', 'others').required(),
+  purchaseType: Joi.string().valid('trial', 'permanent').required(),
+  part: Joi.string().required(),
+  selectedDate: Joi.number().required(),
+});
+
+export const validateInput = (req, res, next) => {
+  const validationResult = userDetailsSchema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({ error: validationResult.error.message });
+  }
+  next();
+};
---
+++ src/models/bodyPart.js
@@ -0,0 +1,54 @@
+// src/models/bodyPart.js
+
+import mongoose from 'mongoose';
+
+// const mongoose = require('mongoose');
+
+const bookformSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: [true, "Please enter your Name"] 
+    },
+
+    phoneNumber: { 
+        type: Number, 
+        required: [true, "Phone number is required"], 
+        unique: true,
+        match: /^[0-9]{10}$/ 
+    },
+
+    email: { 
+        type: String, 
+        required: [true, "Mention your Email address"], 
+        unique: true,
+        match:/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/
+    },
+
+    gender:{
+        type: String,
+        required: [true, "Select your Gender"]
+    },
+
+    purchaseType:{
+        type: String,
+        required: [true, "Select one"]
+    },
+
+    part: { 
+        type: String, 
+        required: [true, "Choose atleast one"] 
+    },
+    
+    selectedDate:{
+        type: Date,
+        required:[true,"Please select your date"]
+    }
+
+});
+
+
+const UserDetailsBookform  = mongoose.model("UserDetailsBookform", bookformSchema);
+
+export  { UserDetailsBookform };
+
+
