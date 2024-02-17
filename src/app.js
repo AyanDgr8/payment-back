595036b0ccddfa0eec6acc4e13fc7434baf9bcb6
Ayan <ayandgr5@gmail.com>
2024-02-17 16:01:21 +0530

    app

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api', router);
+app.use('/', router);
 
 // Middleware for handling 404 errors
 app.use(notFoundHandler);
